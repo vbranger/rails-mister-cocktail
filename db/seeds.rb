ff9b08503b9a7b435cfe589fc1b4b6eb1fdf28ec
Victor Branger <victor.branger@gmail.com>
2020-11-12 12:20:09 +0100

    cocktails routes + controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+serialized_ingredients = open(url).read
+ingredients = JSON.parse(serialized_ingredients)
+
+
+puts 'creating ingredients'
+ingredients["drinks"].each do |ingredient|
+  new_ingredient = Ingredient.new(
+    name: ingredient["strIngredient1"]
+  )
+  new_ingredient.save!
+end
+puts 'ingredients created'
