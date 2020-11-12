b5b347a01668f96b80f505544cdfdc0a790a3584
Victor Branger <victor.branger@gmail.com>
2020-11-12 12:54:28 +0100

    features see list of cocktails & see details with dose needed for each ingredient added

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,11 @@
 class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @doses = Dose.where(cocktail_id: params[:id])
+    @cocktail = Cocktail.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [ :index, :show, :new, :create]
+  resources :cocktails, only: [ :index, :show, :new, :create] do
+    resources :doses, only: [:index]
+  end
 end
