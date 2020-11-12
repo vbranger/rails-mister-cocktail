ec8501a13b93cba6a9eba94f2f04709d1821cc74
Victor Branger <victor.branger@gmail.com>
2020-11-12 14:06:30 +0100

    added adding and deleting doses features

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -8,4 +8,22 @@ class CocktailsController < ApplicationController
     @doses = Dose.where(cocktail_id: params[:id])
     @cocktail = Cocktail.find(params[:id])
   end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
 end
